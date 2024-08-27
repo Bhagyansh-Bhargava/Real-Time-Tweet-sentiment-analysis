dce067823721dd3807dc49193410f2ab3392c084
Bhagyansh-Bhargava <bhagyansh.bhargava@gmail.com>
2024-08-27 22:53:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3e7b21b7060c09ddb1d9bdbfb76c833b585dccb864bde766e802062a2d7bee4a
+size 36
---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b288af4c303286a683e74160e251e9141f6aa141caed1ac23941f8606820045b
+size 10685
